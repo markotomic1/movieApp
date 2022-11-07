10bf7dedfa35eb7cd209ee2099c5bc2690777fc0
Marko <tomicmark3@gmail.com>
2022-11-07 18:11:57 +0100

    finished design

--- movie-app-design/src/App.jsx
+++ movie-app-design/src/App.jsx
@@ -1,8 +1,11 @@
-import Home from "./home/Home";
+import Home from "./pages/home/Home";
 import "./app.scss";
+import Watch from "./pages/watch/Watch";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 
 function App() {
-  return <Home />;
+  return <Login />;
 }
 
 export default App;
